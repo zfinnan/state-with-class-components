12b1846b6f94aca225133b34b23a585a87b68236
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-09 11:38:33 -0500

    add input functionality to counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      count: 0,
+      inputtedNumber: 0
+    }
+
+  }
+
+  increment = () => {
+  const newCount = this.state.count + this.state.inputtedNumber
+  this.setState({ count: newCount })
+  }
+
+  decrement = () => {
+    const newCount = this.state.count - this.state.inputtedNumber
+    this.setState({ count: newCount })
+  }
+
+  changeInput = (evt) => {
+    // console.log(evt.target.value);
+    let newValue = parseInt(evt.target.value) || 0
+
+    this.setState({inputtedNumber: newValue})
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.count}</h1>
+        <input value={this.state.inputtedNumber} onChange={this.changeInput}/>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )
+  }
 }
 
 export default App;
